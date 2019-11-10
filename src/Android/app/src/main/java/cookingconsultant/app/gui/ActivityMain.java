525e7ce73eb358e239687f55c3b2e0376f260153
scolak1 <salih.colak@smail.th-koeln.de>
2019-11-10 21:17:01 +0100

    Hauptseite Design erstellt

--- src/Android/app/src/main/java/cookingconsultant/app/gui/ActivityMain.java
+++ src/Android/app/src/main/java/cookingconsultant/app/gui/ActivityMain.java
@@ -2,14 +2,32 @@ package cookingconsultant.app.gui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import cookingconsultant.app.R;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new RezeptFragment(),"Rezept");
+        adapter.addFragment(new EinkaufFragment(),"Einkaufsliste");
+        adapter.addFragment(new KochlexikonFragment(),"Kochlexikon");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ src/Android/app/src/main/java/cookingconsultant/app/gui/RezeptFragment.java
@@ -0,0 +1,23 @@
+package cookingconsultant.app.gui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cookingconsultant.app.R;
+
+public class RezeptFragment extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.rezept_fragment,container,false);
+        return view;
+    }
+}
