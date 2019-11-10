7bad8d2eed30d86c4c62edbcb2ea555823201b56
scolak1 <salih.colak@smail.th-koeln.de>
2019-11-10 19:03:14 +0100

    initial commit der App

---
+++ src/Android/app/src/main/java/cookingconsultant/app/gui/ActivityMain.java
@@ -0,0 +1,15 @@
+package cookingconsultant.app.gui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cookingconsultant.app.R;
+
+public class ActivityMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
