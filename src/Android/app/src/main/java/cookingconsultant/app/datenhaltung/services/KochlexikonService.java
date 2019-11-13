26cd3c2496704929e9cf3d463c122f7545204b75
scolak1 <salih.colak@smail.th-koeln.de>
2019-11-13 13:49:07 +0100

    Update Methoden

--- src/Android/app/src/main/java/cookingconsultant/app/datenhaltung/services/KochlexikonService.java
+++ src/Android/app/src/main/java/cookingconsultant/app/datenhaltung/services/KochlexikonService.java
@@ -1,4 +1,12 @@
 package cookingconsultant.app.datenhaltung.services;
 
+import java.util.List;
+
+import cookingconsultant.app.datenhaltung.entities.Beitrag;
+
 public interface KochlexikonService {
+
+    public Beitrag getBeitragByID(Integer beitid);
+    public List<Beitrag> getBeitraegeByKategorie(String kategorie);
+
 }
