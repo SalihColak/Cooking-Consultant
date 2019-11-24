92dc7936f6de1b3a8b420603fa5886be09437895
phadaia <philipp_yousif.hadaia@smail.th-koeln.de>
2019-11-25 00:35:46 +0100

    Alle relevanten get-Befehle

---
+++ php/getZutatByID.php
@@ -0,0 +1,36 @@
+<?php
+ include 'db_con.php';
+ $con = openCon();
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$zutid = $_POST['zutid'];
+
+$sql = "SELECT * FROM zutat where zutid='$zutid'";
+ 
+// Confirm there are results
+if ($result = mysqli_query($con, $sql))
+{
+	// We have results, create an array to hold the results
+        // and an array to hold the data
+	$resultArray = array();
+	$tempArray = array();
+ 
+	// Loop through each result
+	while($row = $result->fetch_object())
+	{
+		// Add each result into the results array
+		$tempArray = $row;
+	    array_push($resultArray, $tempArray);
+	}
+ 
+	// Encode the array to JSON and output the results
+	echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
