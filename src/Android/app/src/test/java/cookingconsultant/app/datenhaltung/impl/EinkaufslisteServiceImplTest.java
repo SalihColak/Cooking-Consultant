c5cccfb046c163ea5611892c6a89e4a525ecc23d
phadaia <philipp_yousif.hadaia@smail.th-koeln.de>
2020-01-14 19:50:53 +0100

    TF07-12 implementiert

--- src/Android/app/src/test/java/cookingconsultant/app/datenhaltung/impl/EinkaufslisteServiceImplTest.java
+++ src/Android/app/src/test/java/cookingconsultant/app/datenhaltung/impl/EinkaufslisteServiceImplTest.java
@@ -1,7 +1,160 @@
 package cookingconsultant.app.datenhaltung.impl;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import cookingconsultant.app.datenhaltung.entities.Einkaufsliste;
+import cookingconsultant.app.datenhaltung.entities.Rezept;
+import cookingconsultant.app.datenhaltung.entities.User;
+import cookingconsultant.app.datenhaltung.entities.ZutatState;
+import cookingconsultant.app.datenhaltung.services.RezeptService;
+import cookingconsultant.app.datenhaltung.services.UserService;
+import cookingconsultant.app.fachlogik.services.Constants;
+
 import static org.junit.Assert.*;
 
 public class EinkaufslisteServiceImplTest {
+    static private EinkaufslisteServiceImpl classToTest = new EinkaufslisteServiceImpl();
+    static private final String URL_GET_EINKAUFSLISTEN_BY_USER_ID = Constants.IP_SERVER + "getEinkaufslisteByUserID.php";
+
+    /**
+     * /TF09/ Korrektes Anzeigen der Einkaufslisten eines Benutzers
+     * Anwendungsfall
+     * /LF06/
+     *
+     * Schritte
+     * Nach Einkaufslisten des Benutzers mit ID 1 suchen
+     *
+     * erwartetes Ergebnis
+     * Liste von Einkaufslisten des Benutzers mit der ID 1 wird als Ergebnis zurückgegeben
+     *
+     * Ergebnis
+     * Liste von Einkaufslisten des Benutzers mit der ID 1 wird als Ergebnis zurückgegeben
+     */
+    @Test
+    public void korrektesAnzeigenEinkaufslisten() throws IOException, JSONException {
+
+        URL myurl = new URL(URL_GET_EINKAUFSLISTEN_BY_USER_ID + "?userid=1");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) myurl.openConnection();
+        httpURLConnection.setDoInput(true);
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.connect();
+
+
+        InputStream is = httpURLConnection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+        String line = "";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String data = stringBuilder.toString();
+
+        JSONArray jsonArray = new JSONArray(data);
+        JSONObject jobj;
+        List<Einkaufsliste> rueckgabe = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            jobj = jsonArray.getJSONObject(i);
+            if (jobj != null) {
+                Einkaufsliste einkaufsliste = new Einkaufsliste(jobj.getInt("einkid"), jobj.getString("zustand"), jobj.getInt("menge"));
+                UserService userService = new UserServiceImpl();
+                User user = userService.getUserByID(jobj.getInt("userid"));
+                einkaufsliste.setUser(user);
+                RezeptService rezeptService = new RezeptServiceImpl();
+                Rezept rezept = rezeptService.getRezeptByID(jobj.getInt("rezid"));
+                einkaufsliste.setRezept(rezept);
+                einkaufsliste.setZutatStateList(classToTest.getEinkaufslisteZutatState(jobj.getInt("einkid")));
+                rueckgabe.add(einkaufsliste);
+            }
+        }
+        assertNotSame(rueckgabe, classToTest.getEinkaufslistenByUserID(1));
+        assertEquals(rueckgabe, classToTest.getEinkaufslistenByUserID(1));
+    }
+
+    /**
+     * /TF10/ Fehlerhaftes Anzeigen einer Einkaufsliste
+     * Anwendungsfall
+     * /LF06/
+     *
+     * Nach Einkaufslisten des Benutzers mit ID 5 suchen (Benutzer existiert nicht in der Datenbank)
+     *
+     * erwartetes Ergebnis
+     * Liste von Einkaufslisten des Benutzers mit der ID 5 wird als Ergebnis zurückgegeben
+     *
+     * Ergebnis
+     * Der Benutzer erhält eine leere Liste, da der Benutzer nicht in der Datenbank vorhanden ist
+     */
+    @Test
+    public void fehlerhaftesAnzeigenEinkaufslisten() throws IOException, JSONException {
+        assertTrue(classToTest.getEinkaufslistenByUserID(1).isEmpty());
+    }
+
+    /**
+     * /TF11/ Korrektes Abarbeiten einer Einkaufsliste
+     * Anwendungsfall
+     * /LF07/
+     *
+     * Schritte
+     * Jede Zutat der Einkaufsliste 1 wird auf true gesetzt
+     *
+     * erwartetes Ergebnis
+     * Alle Zustände der Einkaufsliste sind true
+     *
+     * Ergebnis
+     * Alle Zustände der Einkaufsliste sind true
+     */
+    @Test
+    public void korrektesAbarbeitenEinkaufsliste() throws IOException, JSONException {
+        List<ZutatState> testObjekt = classToTest.getEinkaufslisteZutatState(1);
+        for(ZutatState z : testObjekt)
+        {
+            assertFalse(z.getState());
+            classToTest.changeZutatState(z.getId(), true);
+        }
+
+        testObjekt = classToTest.getEinkaufslisteZutatState(1);
+        for(ZutatState z : testObjekt)
+            assertTrue(z.getState());
+    }
+
+    /**
+     * /TF12/ Fehlerhaftes Abarbeiten einer Einkaufsliste
+     * Anwendungsfall
+     * /LF07/
+     *
+     * Schritte
+     * Jede Zutat der Einkaufsliste 1 wird auf false gesetzt
+     *
+     * erwartetes Ergebnis
+     * Alle Zustände der Einkaufsliste sind true
+     *
+     * Ergebnis
+     * Alle Zustände der Einkaufsliste sind false
+     */
+    @Test
+    public void fehlerhaftesAbarbeitenEinkaufsliste() throws IOException, JSONException {
+        List<ZutatState> testObjekt = classToTest.getEinkaufslisteZutatState(2);
+        for(ZutatState z : testObjekt)
+        {
+            assertFalse(z.getState());
+            classToTest.changeZutatState(z.getId(), false);
+        }
+
+        testObjekt = classToTest.getEinkaufslisteZutatState(2);
+        for(ZutatState z : testObjekt)
+            assertFalse(z.getState());
+    }
 }
--- src/Android/app/src/test/java/cookingconsultant/app/datenhaltung/impl/RezeptServiceImplTest.java
+++ src/Android/app/src/test/java/cookingconsultant/app/datenhaltung/impl/RezeptServiceImplTest.java
@@ -16,13 +16,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import cookingconsultant.app.datenhaltung.entities.Rezept;
+import cookingconsultant.app.datenhaltung.entities.Zutat;
+import cookingconsultant.app.datenhaltung.services.ZutatService;
 import cookingconsultant.app.fachlogik.services.Constants;
 
 import static org.junit.Assert.*;
 
 public class RezeptServiceImplTest {
     static private RezeptServiceImpl classToTest = new RezeptServiceImpl();
-    private static final String URL_GET_REZEPTE_BY_QUERY = Constants.IP_SERVER+"getRezeptByArtAnlassPraeferenz.php";
+    static private final String URL_GET_REZEPTE_BY_QUERY = Constants.IP_SERVER+"getRezeptByArtAnlassPraeferenz.php";
+    static private final String URL_GET_REZEPT_BY_ID = Constants.IP_SERVER+"getRezeptByID.php";
 
     /**
      * /TF03/ Korrektes Suchen von Rezepten
@@ -132,5 +135,79 @@ public class RezeptServiceImplTest {
 
         String query = "'BRUNCH';'ESSEN ZU ZWEIT';'UGANDISCH'";
         assertTrue(classToTest.getRezepteByQuery(query).isEmpty());
+    }
+
+    /**
+     * /TF08/ Fehlerhaftes Anzeigen eines Rezepts
+     * Anwendungsfall
+     * /LF04/
+     *
+     * Schritte
+     * Neues Rezept mit einer ID 5 anlegen
+     *
+     * Nach diesem Rezept suchen
+     *
+     * erwartetes Ergebnis
+     * Rezept mit der ID 5 wird als Ergebnis zurückgegeben
+     *
+     * Ergebnis
+     * Der Benutzer erhält eine Null-Referenz, da das Rezept nicht in der Datenbank vorhanden ist
+     */
+    @Test
+    public void fehlerhaftesRezeptAnzeigen() throws IOException, JSONException {
+        URL myurl = new URL(URL_GET_REZEPT_BY_ID+"?rezid=1");
+        HttpURLConnection httpURLConnection = (HttpURLConnection)myurl.openConnection();
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.connect();
+
+
+        InputStream is = httpURLConnection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+        String line ="";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine())!=null){
+            stringBuilder.append(line);
+        }
+        String data = stringBuilder.toString();
+
+        JSONArray jsonArray = new JSONArray(data);
+        JSONObject jobj = jsonArray.getJSONObject(0);
+
+
+        List<String> schritte;
+        schritte = Arrays.asList(jobj.getString("schritte").split(";"));
+
+        Rezept rueckgabe = null;
+        if(jobj!=null) {
+            rueckgabe = new Rezept(jobj.getInt("rezid"), jobj.getString("name"), jobj.getString("beschreibung"), schritte,
+                    jobj.getString("art"), jobj.getString("anlass"), jobj.getString("praeferenz"), jobj.getString("kochzeit") + "min",
+                    jobj.getString("bild"), jobj.getString("menge"));
+            ZutatService zutatService = new ZutatServiceImpl();
+            List<Zutat> zutatList = zutatService.getZutatenByRezeptID(1);
+            rueckgabe.setZutaten(zutatList);
+        }
+        assertNotSame(rueckgabe, classToTest.getRezeptByID(1));
+        assertEquals(rueckgabe, classToTest.getRezeptByID(1));
+    }
+
+    /**
+     * /TF07/ Korrektes Anzeigen eines Rezepts
+     * Anwendungsfall
+     * /LF04/
+     *
+     * Schritte
+     * Rezept mit ID 1 suchen
+     *
+     * erwartetes Ergebnis
+     * Rezept mit der ID 1 wird als Ergebnis zurückgegeben
+     *
+     * Ergebnis
+     * Rezept mit der ID 1 wird als Ergebnis zurückgegeben
+     */
+    @Test
+    public void korrektesRezeptAnzeigen() throws IOException, JSONException {
+        assertNull(classToTest.getRezeptByID(5));
     }
 }
